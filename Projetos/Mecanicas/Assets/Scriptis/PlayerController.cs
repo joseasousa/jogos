a29643e3d0fdab81893b14a1d89da295f5ea4bcf
jose alves sousa neto <a.sousajose@gmail.com>
2016-12-03 13:58:36 -0300

    mov

--- Projetos/Mecanicas/Assets/Scriptis/PlayerController.cs
+++ Projetos/Mecanicas/Assets/Scriptis/PlayerController.cs
@@ -1,50 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
-	private float movX = 3;
+    private float movX = 3;
 
-	public float speed = 40.0f;
-	public float forceJump = 300f;
+    public float speed = 40.0f;
+    public float forceJump = 300f;
 
-	private bool isRigth;
+    private bool isRigth;
 
-	private Transform t;
+    private Transform t;
 
-	// Use this for initialization
-	void Start ()
-	{
-		t = GetComponent <Transform> ();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		movX = Input.GetAxis ("Horizontal");
+    Rigidbody2D rb;
 
-		Vector2 mov = new Vector2 (movX * speed, 0);
+    // Use this for initialization
+    void Start()
+    {
+        t = GetComponent <Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-		//GetComponent<Rigidbody2D> ().AddForce (mov);
-		GetComponent<Rigidbody2D> ().velocity = mov;
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            movX = Input.GetAxis("Horizontal");  
+            rb.velocity = new Vector2(movX * speed, 0);
+        }	
 
-		if (Input.GetButtonDown ("Jump")) {
-			//por velociadade
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, forceJump);
-			//GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, forceJump));
-		}
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(0, forceJump);		
+        }
 
-		if (isRigth && movX > 0) {
-			Flip ();
-		} else if (!isRigth && movX < 0) {
-			Flip ();
-		}
-	
-	}
+        if (isRigth && movX > 0)
+        {
+            Flip();
+        }
+        else if (!isRigth && movX < 0)
+        {
+            Flip();
+        }
 
-	void Flip ()
-	{
-		t.Rotate (new Vector3 (0, 180, 0));
-		isRigth = !isRigth;
-	}
+        GetComponent <Animator>().SetFloat("speed", Math.Abs(movX));
+    }
+
+    void Flip()
+    {
+        t.Rotate(new Vector3(0, 180, 0));
+        isRigth = !isRigth;
+    }
 }
